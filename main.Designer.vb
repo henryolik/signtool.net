b9146b65bc3ffa67ae1e080e7531fa1ad798da10
henryolik <jindrarouball@seznam.cz>
2018-01-31 18:19:38 +0100

    Initial release

---
+++ main.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tb_tosign = New System.Windows.Forms.TextBox()
+        Me.tb_cert = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.tb_name = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.tb_www = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "File to sign:"
+        '
+        'tb_tosign
+        '
+        Me.tb_tosign.Location = New System.Drawing.Point(79, 27)
+        Me.tb_tosign.Name = "tb_tosign"
+        Me.tb_tosign.Size = New System.Drawing.Size(368, 20)
+        Me.tb_tosign.TabIndex = 1
+        '
+        'tb_cert
+        '
+        Me.tb_cert.Location = New System.Drawing.Point(79, 53)
+        Me.tb_cert.Name = "tb_cert"
+        Me.tb_cert.Size = New System.Drawing.Size(368, 20)
+        Me.tb_cert.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(57, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Certificate:"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(463, 24)
+        Me.MenuStrip1.TabIndex = 6
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(16, 132)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(431, 35)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "SIGN!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'tb_name
+        '
+        Me.tb_name.Location = New System.Drawing.Point(79, 79)
+        Me.tb_name.Name = "tb_name"
+        Me.tb_name.Size = New System.Drawing.Size(368, 20)
+        Me.tb_name.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(13, 82)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Name:"
+        '
+        'tb_www
+        '
+        Me.tb_www.Location = New System.Drawing.Point(79, 106)
+        Me.tb_www.Name = "tb_www"
+        Me.tb_www.Size = New System.Drawing.Size(368, 20)
+        Me.tb_www.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(13, 109)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(49, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Website:"
+        '
+        'main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(463, 172)
+        Me.Controls.Add(Me.tb_www)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.tb_name)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.tb_cert)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.tb_tosign)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "main"
+        Me.Text = "SignTool.NET v1.0.1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents tb_tosign As System.Windows.Forms.TextBox
+    Friend WithEvents tb_cert As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents tb_name As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents tb_www As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ main.vb
@@ -0,0 +1,45 @@
+﻿Public Class main
+
+    Private Sub AboutToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles AboutToolStripMenuItem.Click
+        About.Show()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As System.Object, e As System.EventArgs) Handles tb_tosign.Click
+        OpenFileDialog1.Title = "Please select a file"
+        OpenFileDialog1.InitialDirectory = Application.StartupPath
+        OpenFileDialog1.Filter = "All files|*.*"
+        OpenFileDialog1.Multiselect = False
+        OpenFileDialog1.FileName = ""
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            tb_tosign.Text = OpenFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As System.Object, e As System.EventArgs) Handles tb_cert.Click
+        OpenFileDialog1.Title = "Please select a signing certificate"
+        OpenFileDialog1.InitialDirectory = Application.StartupPath
+        OpenFileDialog1.Filter = "Certificate files|*.pfx"
+        OpenFileDialog1.Multiselect = False
+        OpenFileDialog1.FileName = ""
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            tb_cert.Text = OpenFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Process.Start("cmd", "/c signtool sign /f """ & tb_cert.Text & """ /t http://timestamp.verisign.com/scripts/timstamp.dll /d """ & tb_name.Text & """ /du " & tb_www.Text & " /v """ & tb_tosign.Text & Chr(34))
+        My.Settings.cert = tb_cert.Text
+        My.Settings.tosign = tb_tosign.Text
+        My.Settings.www = tb_www.Text
+        My.Settings.name = tb_name.Text
+    End Sub
+
+    Private Sub main_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        tb_cert.Text = My.Settings.cert
+        tb_name.Text = My.Settings.name
+        tb_tosign.Text = My.Settings.tosign
+        tb_www.Text = My.Settings.www
+    End Sub
+End Class
